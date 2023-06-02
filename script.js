b334baa164b110fbafb3d835a7f5ad30917bae95
Kreoniz <kreoniz@mail.ru>
2023-06-02 12:03:13 +0600

    Render all "Book" objects on page

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let myLibrary = [];
+
 function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
@@ -8,3 +10,48 @@ function Book(title, author, pages, read) {
         return `${title} by ${author}, ${pages} pages, ${read ? "read" : "not read yet"}`;
     }
 }
+
+function addBookToLibrary(title, author, pages, read) {
+    const book = new Book(title, author, pages, read);
+    myLibrary.push(book);
+}
+
+addBookToLibrary("Title", "Author", 100, false);
+addBookToLibrary("The Hobbit", "J. R. R. Tolkien",  295, false);
+addBookToLibrary("Demian", "Hermann Hesse",  390, true);
+addBookToLibrary("Think Like a Programmer", "Anton Spraul",  260, false);
+
+const library = document.querySelector("#library");
+// Render all objects from the array on page
+function renderLibrary() {
+    for (const book of myLibrary) {
+        const card = createBookCard(book.title, book.author, book.pages, book.read);
+        library.appendChild(card)
+    }
+}
+
+// Create an HTML element with book info
+function createBookCard(title, author, pages, read) {
+    const card = document.createElement("div");
+    const cardTitle = document.createElement("div");
+    const cardAuthor = document.createElement("div");
+    const cardPages = document.createElement("div");
+    const cardStatus = document.createElement("div");
+
+    cardTitle.textContent = title;
+    card.appendChild(cardTitle);
+
+    cardAuthor.textContent = author;
+    card.appendChild(cardAuthor);
+
+    cardPages.textContent = pages;
+    card.appendChild(cardPages);
+
+    cardStatus.textContent = read ? "Read" : "Not read";
+    card.appendChild(cardStatus);
+
+    card.classList.add("book-card");
+    return card;
+}
+
+renderLibrary();
