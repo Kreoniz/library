6d10a329861cb51100916f674519ec1cba0ff399
Kreoniz <kreoniz@mail.ru>
2023-06-04 18:51:09 +0600

    Add a button for removing books

--- script.js
+++ script.js
@@ -26,8 +26,11 @@ const library = document.querySelector("#library");
 function renderLibrary(myLibrary) {
     library.innerHTML = "";
 
-    for (const book of myLibrary) {
+    for (let i = 0; i < myLibrary.length; i++) {
+        const book = myLibrary[i];
         const card = createBookCard(book.title, book.author, book.pages, book.read);
+        card.querySelector("button").dataset.index = i;
+        card.dataset.index = i;
         library.appendChild(card)
     }
 }
@@ -39,18 +42,36 @@ function createBookCard(title, author, pages, read) {
     const cardAuthor = document.createElement("div");
     const cardPages = document.createElement("div");
     const cardStatus = document.createElement("div");
+    const removeBtn = document.createElement("button");
 
-    cardTitle.textContent = title;
+    cardTitle.textContent = `"${title}"`;
+    cardTitle.classList.add("book-card-title");
     card.appendChild(cardTitle);
 
-    cardAuthor.textContent = author;
+    cardAuthor.textContent = `by ${author}`;
+    cardAuthor.classList.add("book-card-author");
     card.appendChild(cardAuthor);
 
-    cardPages.textContent = pages;
+    cardPages.textContent = `${pages} pages`;
+    cardPages.classList.add("book-card-pages");
     card.appendChild(cardPages);
 
     cardStatus.textContent = read ? "Read" : "Not read";
+    cardStatus.classList.add("book-card-status");
     card.appendChild(cardStatus);
+
+    removeBtn.textContent = "Remove the book";
+    removeBtn.classList.add("remove-button");
+    removeBtn.id = "removeBtn";
+    removeBtn.addEventListener("click", e => {
+        const index = removeBtn.dataset.index;
+        console.log(index);
+        myLibrary.splice(index, 1);
+
+        renderLibrary(myLibrary);
+    });
+
+    card.appendChild(removeBtn);
 
     card.classList.add("book-card");
     return card;
